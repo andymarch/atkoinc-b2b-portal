c0391f94533f520012f885edd7a4fdef72077ae3
Andy March <mail@andymarch.co.uk>
2019-08-02 14:57:51 +0100

    Account show and upated

--- routes/users.js
+++ routes/users.js
@@ -27,6 +27,50 @@ module.exports = function (_oidc){
         res.render('error', { title: 'Error' });
         }  
     });
+
+    router.get('/:id', oidc.ensureAuthenticated(), async function(req, res, next) {
+        try {
+            var response = await axios.get(process.env.TENANT_URL+'/api/v1/users/'+req.params.id);
+            var targetUser = new UserModel(response.data)
+            res.render('user', { title: 'Users', user: req.userContext.userinfo.given_name, targetUser:targetUser});
+        }
+        catch(err) {
+            console.log(err)
+            // set locals, only providing error in development
+            res.locals.message = err.message;
+            res.locals.error = req.app.get('env') === 'development' ? err : {};
+    
+            // render the error page
+            res.status(err.status || 500);
+            res.render('error', { title: 'Error' });
+            }  
+        });
+
+    router.post('/:id', oidc.ensureAuthenticated(), async function(req, res, next) {
+            try {
+                await axios.post(process.env.TENANT_URL+'/api/v1/users/'+req.params.id,
+                {
+                    profile: { 
+                        firstName: req.body.firstname,
+                        lastName: req.body.secondname,
+                        organization: req.body.organization,
+                        email: req.body.email,
+                        login: req.body.email
+                    }
+                });
+                res.redirect(req.params.id)
+            }
+            catch(err) {
+                console.log(err)
+                // set locals, only providing error in development
+                res.locals.message = err.message;
+                res.locals.error = req.app.get('env') === 'development' ? err : {};
+        
+                // render the error page
+                res.status(err.status || 500);
+                res.render('error', { title: 'Error' });
+                }  
+            });
   return router;
 }
 
