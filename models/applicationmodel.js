9348cbf64b4cc0124b6879ea0d09dc56106f94dd
Andy March <mail@andymarch.co.uk>
2019-08-07 11:16:57 -0500

    Dumping the applinks for dashboard

---
+++ models/applicationmodel.js
@@ -0,0 +1,9 @@
+
+class ApplicationModel {
+    constructor(title,link) {
+        this.title = title
+        this.link = link
+    }
+}
+
+module.exports = ApplicationModel
--- routes/dashboard.js
+++ routes/dashboard.js
@@ -1,12 +1,38 @@
 const express = require('express');
 const router = express.Router();
 var oidc = require('@okta/oidc-middleware');
+const axios = require('axios');
+var ApplicationModel = require('../models/applicationmodel')
 
 module.exports = function (_oidc){
   oidc = _oidc;
 
-router.get('/', oidc.ensureAuthenticated(), function(req, res, next) {
-  res.render('dashboard', { title: 'Things'});
+router.get('/', oidc.ensureAuthenticated(), async function(req, res, next) {
+  try {
+  var response = await axios.get(process.env.TENANT_URL+'/api/v1/apps?filter=user.id+eq+\"'+req.userContext.userinfo.sub+'\"')
+        var apps = [];
+        response.data.forEach(function(entry) {
+            try{
+            if(entry.settings.oauthClient.initiate_login_uri){
+                apps.push(new ApplicationModel(entry.label,entry.settings.oauthClient.initiate_login_uri))
+            }   
+            }
+            catch(error){
+                //do nothing
+            }
+        })
+  res.render('dashboard', { title: 'Things',applinks: apps});
+  }
+  catch(err) {
+    console.log(err)
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error', { title: 'Error' });
+    }  
 });
 
   return router;
